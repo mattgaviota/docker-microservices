27656e35aed0e66ff6d22c52a69c5481447712fb
Javier Ocampo <ferroxido@gmail.com>
2020-04-04 22:40:11 -0300

    Dockerize node service

--- api-node/src/index.js
+++ api-node/src/index.js
@@ -4,7 +4,7 @@ const http = require('http')
 const app = require('./app')
 
 const server = http.createServer(app)
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8001
 
 server.listen(port, () => {
   console.log(`Server listen on port ${port}`)
