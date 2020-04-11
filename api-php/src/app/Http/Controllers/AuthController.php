10a02abc5a652fcc01a444e61095b3d3f48f96a6
Matias Novoa <matias.novoa.gundel@gmail.com>
2020-04-11 12:10:18 -0400

    Added swagger docs for user service

--- api-php/src/app/Http/Controllers/AuthController.php
+++ api-php/src/app/Http/Controllers/AuthController.php
@@ -53,15 +53,33 @@ class AuthController extends Controller
      *     @OA\Response(
      *          response="200",
      *          description="Json web Token",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="data",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="errors",
+     *                     @OA\Schema(
+     *                        type="array",
+     *                        @OA\Items(type="string"),
+     *                      ),
+     *                 ),
+     *                 example={"data": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9...", "errors": "[]"}
+     *             )
+     *         )
      *     ),
      *     @OA\Response(response=400, description="Email or Password is wrong"),
+     *     @OA\Response(response=422, description="Validation Error"),
      * )
      */
     public function login(Request $request)
     {
         $errors = $this->validator->authenticateValidation($request);
         if (! empty($errors)) {
-            return response()->json(['data' => [], 'errors' => $errors], 200);
+            return response()->json(['data' => [], 'errors' => $errors], 422);
         }
         try {
             $user = User::where('email', $request->email)->firstOrFail();
@@ -76,11 +94,117 @@ class AuthController extends Controller
         }
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/validate",
+     *     @OA\Response(
+     *          response="200",
+     *          description="Logged User",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="data",
+     *                     @OA\Schema(
+     *                         @OA\Property(
+     *                             property="id",
+     *                             type="string"
+     *                         ),
+     *                         @OA\Property(
+     *                             property="name",
+     *                             type="string"
+     *                         ),
+     *                         @OA\Property(
+     *                             property="email",
+     *                             type="string"
+     *                         ),
+     *                     ),
+     *                 ),
+     *                 @OA\Property(
+     *                     property="errors",
+     *                     @OA\Schema(
+     *                        type="array",
+     *                        @OA\Items(type="string"),
+     *                      ),
+     *                 ),
+     *                 example={"data": {"id": 1, "name": "Matt", "email": "matt@test.test"}, "errors": "[]"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response=401, description="User could not be validated"),
+     *     @OA\Response(response=422, description="Validation Error"),
+     * )
+     */
     public function validateUser(Request $request)
     {
         return response()->json(['data' => $request->auth, 'errors' => []], 200);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/signin",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="confirmed_password",
+     *                     type="string"
+     *                 ),
+     *                 example={"name": "Matias Novoa", "email": "test@test.test", "password": "12345", "confirmed_password": "12345"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *          response="200",
+     *          description="Logged User",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="data",
+     *                     @OA\Schema(
+     *                         @OA\Property(
+     *                             property="id",
+     *                             type="string"
+     *                         ),
+     *                         @OA\Property(
+     *                             property="name",
+     *                             type="string"
+     *                         ),
+     *                         @OA\Property(
+     *                             property="email",
+     *                             type="string"
+     *                         ),
+     *                     ),
+     *                 ),
+     *                 @OA\Property(
+     *                     property="errors",
+     *                     @OA\Schema(
+     *                        type="array",
+     *                        @OA\Items(type="string"),
+     *                      ),
+     *                 ),
+     *                 example={"data": {"id": 1, "name": "Matt", "email": "matt@test.test"}, "errors": "[]"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response=409, description="User could not be created"),
+     *     @OA\Response(response=422, description="Validation Error"),
+     * )
+     */
     public function signin(Request $request)
     {
         $errors = $this->validator->createUserValidation($request);
