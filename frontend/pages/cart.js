079116be8fc93f72fdc9365fecb00f1b5e340146
Javier Ocampo <ferroxido@gmail.com>
2020-07-12 00:17:29 -0300

    Cart page

--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -2,47 +2,61 @@ import React, { useState, useEffect } from 'react'
 import Error from 'next/error'
 import Layout from '../components/Layout'
 import { handleAuthSSR } from '../lib/auth'
+import { getCart } from '../lib/cart'
 
 function CartPage ({ user }) {
+  const [groups, setGroups] = useState({})
+
+  useEffect(() => {
+    const cart = getCart()
+    const groups = cart.reduce((r, i) => {
+      r[i.seller] = [...r[i.seller] || [], i]
+      return r
+    }, {})
+    setGroups(groups)
+  }, [])
+
   if (!user || user.usertype !== 'buyer') {
     return <Error statusCode={403} />
   }
 
   return (
     <Layout title='Cart'>
-      <div classNameName='ui container'>
-        <h2 classNameName='ui header'>Cart</h2>
+      <div className='ui container'>
+        <h2 className='ui header'>Create Orders</h2>
         <div className='ui cards'>
-          <div className='card'>
-            <div className='content'>
-              <div className='header'>Elliot Fu</div>
-              <div className='meta'>Seller</div>
-              <div className='description'>
-                <table className='ui table'>
-                  <thead>
-                    <tr>
-                      <th>Item</th>
-                      <th>Quantity</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>Item name</td>
-                      <td>Quantity</td>
-                    </tr>
-                    <tr>
-                      <td>Quantity</td>
-                      <td>Quantity</td>
-                    </tr>
-                  </tbody>
-                </table>
+          {Object.keys(groups).map(group => (
+            <div className='card' key={group}>
+              <div className='content'>
+                <div className='header'>{group}</div>
+                <div className='meta'>Seller</div>
+                <div className='description'>
+                  <table className='ui table'>
+                    <thead>
+                      <tr>
+                        <th>Item</th>
+                        <th>Quantity</th>
+                        <th>Category</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {groups[group].map(item => (
+                        <tr key={item.id}>
+                          <td>{item.name}</td>
+                          <td>{item.quantity}</td>
+                          <td>{item.category}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <div className='ui bottom attached button'>
+                <i className='add icon' />
+                Create Order
               </div>
             </div>
-            <div className='ui bottom attached button'>
-              <i className='add icon' />
-              Create Order
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </Layout>
