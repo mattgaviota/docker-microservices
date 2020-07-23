9223aed6757e859dcc1d43d7cf686d20738a88da
Javier Ocampo <ferroxido@gmail.com>
2020-07-22 21:07:51 -0300

    Add favicon

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -14,6 +14,7 @@ class MyDocument extends Document {
             name='viewport'
             content='width=device-width, initial-scale=1.0'
           />
+          <link rel='icon' href='/public/favicon.ico' />
           <style jsx global>{`
             body {
               margin: 0;
